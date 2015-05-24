863d5e18b1658dba9e359c6bce93d646ab4366ba
Jesse Monroy <jessemonroy650@yahoo.com>
2015-05-24 00:03:48 -0700

    added error message for error.code

--- capture.js
+++ capture.js
@@ -4,6 +4,15 @@
 
 var captureCallback = {};
 
+var ErrorCode = {
+
+    CaptureError.CAPTURE_INTERNAL_ERR: "The camera or microphone failed to capture image or sound.",
+    CaptureError.CAPTURE_APPLICATION_BUSY: "The camera or audio capture application is currently serving another capture request.",
+    CaptureError.CAPTURE_INVALID_ARGUMENT: "Invalid use of the API (e.g., the value of limit is less than one).",
+    CaptureError.CAPTURE_NO_MEDIA_FILES: "The user exits the camera or audio capture application before capturing anything.",
+    CaptureError.CAPTURE_NOT_SUPPORTED: "The requested capture operation is not supported."
+}
+
 	function captureSuccess(mediaFiles) {
 		captureCallback(mediaFiles[0]);
 /*        var i, len;
@@ -16,7 +25,7 @@ var captureCallback = {};
     // Called if something bad happens.
     //
     function captureError(error) {
-		alert(error.code);
+		alert(ErrorCode[error.code]);
 /*        var msg = 'An error occurred during capture: ' + error.code;
         navigator.notification.alert(msg, null, 'Uh oh!');
 */
