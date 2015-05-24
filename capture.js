dda4b4f7d5def731835c6cf09a05ff0091e3ae73
Jesse Monroy <jessemonroy650@yahoo.com>
2015-05-24 00:06:32 -0700

    change array type

--- capture.js
+++ capture.js
@@ -4,14 +4,13 @@
 
 var captureCallback = {};
 
-var ErrorCode = {
-
-    CaptureError.CAPTURE_INTERNAL_ERR: "The camera or microphone failed to capture image or sound.",
-    CaptureError.CAPTURE_APPLICATION_BUSY: "The camera or audio capture application is currently serving another capture request.",
-    CaptureError.CAPTURE_INVALID_ARGUMENT: "Invalid use of the API (e.g., the value of limit is less than one).",
-    CaptureError.CAPTURE_NO_MEDIA_FILES: "The user exits the camera or audio capture application before capturing anything.",
-    CaptureError.CAPTURE_NOT_SUPPORTED: "The requested capture operation is not supported."
-}
+var ErrorCode = [
+"The camera or microphone failed to capture image or sound.",
+"The camera or audio capture application is currently serving another capture request.",
+"Invalid use of the API (e.g., the value of limit is less than one).",
+"The user exits the camera or audio capture application before capturing anything.",
+"The requested capture operation is not supported."
+];
 
 	function captureSuccess(mediaFiles) {
 		captureCallback(mediaFiles[0]);
